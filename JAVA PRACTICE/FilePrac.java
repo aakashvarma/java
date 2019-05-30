2028a4fcf8ebc3535a673f2d5c4eb34fe38e2a97
aakashvarma <aakashvarma18@gmail.com>
2019-05-30 11:44:17 +0530

    file prac

--- JAVA PRACTICE/FilePrac.java
+++ JAVA PRACTICE/FilePrac.java
@@ -1,37 +1,21 @@
 import java.io.*;
 
-class Employee implements Serializable{
-    int empId;
-    transient String empName;
-    Employee(){
-
-    }
-    Employee(int empId, String empName){
-        this.empId = empId;
-        this.empName = empName;
-    }
-}
-
 class FilePrac{
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws IOException{
+        
         try{
-            FileOutputStream out = new FileOutputStream("serialize.ser");
-            ObjectOutputStream objout = new ObjectOutputStream(out);
+            FileInputStream in = new FileInputStream("outtest.txt");
+            BufferedInputStream bi = new BufferedInputStream(in);
 
-            Employee obj = new Employee(101, "Varma");
-
-            objout.writeObject(obj);
-            System.out.println("Object successfully written.");
-
-
-            FileInputStream in = new FileInputStream("serialize.ser");
-            ObjectInputStream objin = new ObjectInputStream(in);
-
-            Employee obj1 = (Employee)objin.readObject();
-            System.out.println(obj1.empId+ " "+obj1.empName);
+            int c = bi.read();
+            while(c!= -1){
+                System.out.print((char)c);
+                c = bi.read();
+            }
 
         }catch(Exception e){
             e.printStackTrace();
         }
+        
     }
 }
