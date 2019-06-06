c49dd1f5b0aedb11fe7e1775b625a0431f387d23
aakashvarma <aakashvarma18@gmail.com>
2019-06-06 12:29:38 +0530

    practice hashmaps added

--- JAVA PRACTICE/ArrList.java
+++ JAVA PRACTICE/ArrList.java
@@ -1,13 +1,30 @@
-import java.util.HashMap;
+import java.util.*;
 
 class ArrList{
     public static void main(String[] args){
         HashMap<Integer, String> H1 = new HashMap<>();
 
-        H1.add(2, "Aakash");
-        H1.add(5, "Varma");
-        H1.add(1, "Qwerty");
-        H1.add(4, "Poiuyt");
-        H1.add(3, "Asdfgh");
+        H1.put(2, "Aakash");
+        H1.put(5, "Varma");
+        H1.put(1, "Qwerty");
+        H1.put(4, "Poiuyt");
+        H1.put(3, "Asdfgh");
+
+        System.out.println(H1);
+
+        Set s = H1.entrySet();
+        System.out.println(s);
+
+        Iterator it = s.iterator();
+
+        while(it.hasNext()){
+            Map.Entry me = (Map.Entry)it.next();
+            System.out.println(me.getValue());
+        }
+
+        System.out.println("_______________");
+        System.out.println(H1.remove(3));
+        System.out.println(H1);
+
     }
 }
