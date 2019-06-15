8852dc90297b6c1f3baad28aaba9858c87d9cc01
aakashvarma <aakashvarma18@gmail.com>
2019-06-15 20:24:43 +0530

    added some practice files

--- JAVA PRACTICE/Clone.java
+++ JAVA PRACTICE/Clone.java
@@ -1,26 +1,30 @@
-class Student18 implements Cloneable{
-    int rollno;
+class Employee implements Cloneable{
+    int id;
     String name;
-    
-    Student18(int rollno,String name){
-        this.rollno=rollno;
-        this.name=name;
+    Employee(int id, String name){
+        this.id = id;
+        this.name = name;
     }
-    
-    public Object clone()throws CloneNotSupportedException{
+
+    public void display(){
+        System.out.println(id + " " + name);
+    }
+
+    protected Object clone() throws CloneNotSupportedException{
         return super.clone();
     }
-    
-    public static void main(String args[]){
+}
+
+class Clone{
+    public static void main(String[] args){
+        Employee obj1 = new Employee(101, "Varma");
         try{
-            Student18 s1=new Student18(101,"amit");
-            
-            Student18 s2=(Student18)s1.clone();
-            
-            System.out.println(s1.rollno+" "+s1.name);
-            System.out.println(s2.rollno+" "+s2.name);
-        
-        }catch(CloneNotSupportedException c){}
+            Employee obj2 = (Employee)obj1.clone();
+            obj1.display();
+            obj2.display();
+            System.out.println(obj1.equals(obj2));
+        }catch(CloneNotSupportedException e){
+            e.printStackTrace();
+        }
     }
 }
-    
--- JAVA PRACTICE/Practice.java
+++ JAVA PRACTICE/Practice.java
@@ -1,28 +1,98 @@
-class A{
-    int y = 10;
-    public int display(){
-        // System.out.println("Class A");
-        // int x =10;
-        return y;
-    }
-}
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.Math;
 
-class B extends A{
-    int y = 20;
-    @Override
-    public int display(){
-        // System.out.println("Class B");
-        // int x = 20;
-        return y;
+// import java.io.FileInputStream;
+// import java.io.FileNotFoundException;
+// import java.io.FileOutputStream;
+// import java.io.IOException;
+// import java.io.ObjectInputStream;
+// import java.io.ObjectOutputStream;
+// import java.io.Serializable;
 
-    }
-}
+// class Employee implements Serializable{
+//     int id;
+//     String name;
+//     Employee(int id, String name){
+//         this.id = id;
+//         this.name = name;
+//     }
+
+//     public void display(){
+//         System.out.println(id +" " + name);
+//     }
+// }
+
+// class Practice{
+//     public static void main(String[] args){
+//         try{
+//             FileOutputStream out = new FileOutputStream("outfile.txt");
+//             ObjectOutputStream objout = new ObjectOutputStream(out);
+
+//             Employee obj1 = new Employee(101, "Aakash");
+
+//             objout.writeObject(obj1);
+
+
+//             FileInputStream in = new FileInputStream("outfile.txt");
+//             ObjectInputStream objin = new ObjectInputStream(in);
+
+//             Employee obj2 = (Employee)objin.readObject();
+
+//             obj2.display();
+//         }catch(FileNotFoundException e){
+//             e.getMessage();
+//         }catch(IOException e){
+//             e.getMessage();
+//         }catch(ClassNotFoundException e){
+//             e.getMessage();
+//         }
+//     }
+// }
+
+
+// class Practice{
+//     public static void main(String[] main) throws IOException{
+//         String finalCount = "";
+//         String text = "world wide web";
+//         String words[] = text.split(" ", 0);
+//         for(int i = 0; i < words.length; i++){
+//             int wordLength = words[i].length();
+//             int temp = 0;
+//             if(wordLength%2 == 0){
+//                 for(int j = 0; j < wordLength/2; j++){
+//                     temp = Math.abs(((int)words[i].charAt(j) - 96) - ((int)words[i].charAt(wordLength - j - 1) - 96)) + temp;
+//                     // System.out.println(words[i].charAt(j) + " " + words[i].charAt(wordLength - j - 1));
+//                 }
+//             }
+//             else{
+//                 for(int j = 0; j < wordLength/2; j++){
+//                     temp = Math.abs(((int)words[i].charAt(j) - 96) - ((int)words[i].charAt(wordLength - j - 1) - 96)) + temp;
+//                     // System.out.println(words[i].charAt(j) + " " + words[i].charAt(wordLength - j - 1));
+//                 }
+//                 temp = temp + (int)words[i].charAt(wordLength/2) - 96;
+//                 // System.out.println(words[i].charAt(wordLength/2));
+//             }
+//             finalCount = finalCount + String.valueOf(temp);
+//         }
+//         // System.out.println(((int)'o'-96) - ((int)'l'-96));
+//         System.out.println(finalCount);
+//     }
+// }
+
 
 class Practice{
     public static void main(String[] args){
-        A ref;
-        ref = new B();
-        System.out.println(ref instanceof B);
-        System.out.println(ref instanceof A);
+        int sum = 0;
+		String strNum = String.valueOf(582109);
+		for(int i = 0; i < strNum.length()-1; i++){
+			int base = Character.getNumericValue(strNum.charAt(i));
+            int power = Character.getNumericValue(strNum.charAt(i+1));
+            double numVal = (int)Math.pow(base, power);
+            sum = sum + (int)numVal;
+            System.out.println(numVal+ " -> "+ i);
+        }
+        System.out.println(strNum.length());
+		System.out.println(sum);
     }
 }
